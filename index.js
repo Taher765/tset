5cfad3698a187fe9dea04178ab767fd30321976d
Taher765 <taher.abozied246@gmail.com>
2024-10-24 01:38:55 +0300

    edit port 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ server.use(middlewares);
 server.use(router);
 
 // تشغيل السيرفر على المنفذ 3000
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
 });
