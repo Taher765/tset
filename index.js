10ef52d3407dc1fcb7bf4e636aef62c5411bc40f
Taher765 <taher.abozied246@gmail.com>
2024-10-25 04:20:06 +0300

    update db json

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json"); // مسار ملف JSON
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const cors = require("cors");
 
-// استخدام الميدلوير (middlewares)
+server.use(cors());
 server.use(middlewares);
-// استخدام الراوتر
+
 server.use(router);
 
-// تشغيل السيرفر على المنفذ 3000
+// run server on port 3000
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
